8371f20133ff2c16ea95e1774bffc4bd393939f7
LucR31 <94859181+LucR31@users.noreply.github.com>
2024-05-29 08:30:09 +0000

    updates in netcdf query

--- widgets/sens_query.py
+++ widgets/sens_query.py
@@ -35,22 +35,21 @@ class SearchSens(widgets.VBox):
             style=style,
         )
         self.model = widgets.Dropdown(
-            options=['NAME',
-                     'FLEXPART'],
+            options=["NAME", "FLEXPART"],
             description="Model",
             ensure_option=True,
             style=style,
         )
         self.model_version = widgets.Dropdown(
-            options=['NAME III (version 7.2)',
-                     'FLEXPART IFS (version 9.1_Empa)'],
+            options=["NAME III (version 7.2)", "FLEXPART IFS (version 9.1_Empa)"],
             description="Model version",
             ensure_option=True,
             style=style,
         )
         self.species = widgets.Dropdown(
-            options=['inert',
-                     ],
+            options=[
+                "inert",
+            ],
             description="Species",
             ensure_option=True,
             style=style,
@@ -60,24 +59,30 @@ class SearchSens(widgets.VBox):
             value="""<hr>
             """
         )
-        search_crit = widgets.HBox([self.location, 
-                                    widgets.GridBox([self.date_range, 
-                                                     self.domain,
-                                                     self.model,
-                                                     self.model_version,
-                                                     self.species],
-                                                     layout=widgets.Layout(grid_template_columns="repeat(2, 50%)"))
-                                                  
-                                                  
-                                                  ])
+        search_crit = widgets.HBox(
+            [
+                self.location,
+                widgets.GridBox(
+                    [
+                        self.date_range,
+                        self.domain,
+                        self.model,
+                        self.model_version,
+                        self.species,
+                    ],
+                    layout=widgets.Layout(grid_template_columns="repeat(2, 50%)"),
+                ),
+            ]
+        )
         button = widgets.Button(description="Search")
-        self.results = widgets.HTML()
         self.info_out = widgets.Output()
+
+        self.acc = widgets.Accordion()
 
         def on_click(b):
             with self.info_out:
                 clear_output()
-                self.search()
+                self.accordions()
 
         button.on_click(on_click)
 
@@ -86,15 +91,35 @@ class SearchSens(widgets.VBox):
                 search_crit,
                 self.ind_title,
                 button,
-                self.results,
+                self.acc,
                 self.info_out,
                 self.ind_title,
             ]
         )
 
-    def search(self):
-        self.results.value = "searching..."
+    def accordions(self):
+        icons = {True: "  ✅", False: "  ❌"}
+        list_locations = {}
+        for i in self.location.value:
+            html, is_complete = self.search(i)
+            children = [html]
+            if is_complete:
+                children.append(
+                    widgets.Button(
+                        description="Run missing",
+                        button_style="info",
+                    )
+                )
+            list_locations[widgets.VBox(children=children)] = (is_complete, i)
 
+        self.acc.children = list(list_locations.keys())
+        i = 0
+        for _, v in list_locations.items():
+            self.acc.set_title(i, f"{v[1]}" + icons[not v[0]])
+            i += 1
+
+    def search(self, n_location):
+        missing_dates = []
         dates_list = utils.simulation_dates_parser([self.date_range.value])
         reformated_dates = list(set([i[:4] + i[5:7] for i in dates_list]))
         qb = QueryBuilder()
@@ -108,16 +133,18 @@ class SearchSens(widgets.VBox):
             filters={
                 "attributes.filename": {
                     "and": [
-                        {"or": [{"like": f"{l}%"} for l in self.location.value]},
+                        {"or": [{"like": f"{n_location}%"}]},
                         {"or": [{"like": f"%{l}%"} for l in reformated_dates]},
                     ]
                 },
-               # "attributes.global_attributes.domain": {"==": f"'{self.domain.value}'"},
-               "attributes.global_attributes.model": {"==": f"'{self.model.value}'"},
-               #"attributes.global_attributes.model_version": {"ilike": f"'{self.model_version.value}'"},
-               "attributes.global_attributes.species": {"ilike": f"'{self.species.value}'"},
+                # "attributes.global_attributes.domain": {"==": f"'{self.domain.value}'"},
+                "attributes.global_attributes.model": {"==": f"'{self.model.value}'"},
+                # "attributes.global_attributes.model_version": {"ilike": f"'{self.model_version.value}'"},
+                "attributes.global_attributes.species": {
+                    "ilike": f"'{self.species.value}'"
+                },
             },
-        )                
+        )
 
         html = """<style>
         table, th, td {
@@ -130,11 +157,12 @@ class SearchSens(widgets.VBox):
                     <th>Date</th>
                     <th>Name</th>
                     <th>Created</th>
-                    
+
                 </tr>"""
-        dict_={}
+
+        dict_ = {}
         for i in qb.all():
-            dict_[i[0][-9:-3]]=i
+            dict_[i[0][-9:-3]] = i
         for m in reformated_dates:
             if m in dict_.keys():
                 html += f"""<tr>
@@ -147,7 +175,7 @@ class SearchSens(widgets.VBox):
                         <td>{m}</td>
                         <td>{dict_[m][0]}</td>
                         <td>{dict_[m][1]}</td>
-                        
+
                         </tr>"""
             else:
                 html += f"""<tr>
@@ -156,7 +184,7 @@ class SearchSens(widgets.VBox):
                         <td><mark style='color: red;'>Missing</mark></td>
                         <td><mark style='color: red;'>Missing</mark></td>
                         </tr>"""
-
+                missing_dates.append(m)
         html += "</table>"
 
-        self.results.value = html
+        return widgets.HTML(html), missing_dates
