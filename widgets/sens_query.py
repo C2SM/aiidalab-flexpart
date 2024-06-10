519b5d7418592cfc9c575799b26270c853f19b14
Fernandez Vilanova, Lucas <lucas.fernandez@empa.ch>
2024-06-10 13:33:01 +0000

    new query of netcdf files

--- widgets/sens_query.py
+++ widgets/sens_query.py
@@ -1,5 +1,6 @@
 import ipywidgets as widgets
-from IPython.display import clear_output
+import re
+from IPython.display import clear_output,display
 
 from aiida.orm import QueryBuilder
 
@@ -8,24 +9,41 @@ from settings import NETCDF
 
 style = {"description_width": "initial"}
 
-
-def search_locations():
+def search_locations(a_obs):
     available_locations = []
     qb = QueryBuilder()
-    qb.append(NETCDF, project="attributes.filename")
+    qb.append(NETCDF, 
+              filters = {'attributes.filename':
+                        {"or": [{"like": f"{l}%"} for l in a_obs]},
+                        #'attributes.global_attributes.model': {'==': 'NAME'}
+               },
+              project="attributes.filename")
     for i in qb.all():
-        available_locations.append(i[0].split("_")[0])
+        s = re.split('_|-',i[0])
+        available_locations.append(s[0]+'-'+s[1])
     return list(set(available_locations))
 
 
 def search_species():
+    available_species = []
     qb = QueryBuilder()
     qb.append(NETCDF, project="attributes.global_attributes.species")
-    return list(set([x[0] for x in qb.all()]))
+    for i in qb.all():
+        if i[0]:
+            values = i[0].split(';')
+            if len(values)>1:
+                for j in range(len(values)):
+                    available_species.append(re.sub("'", '',values[j]))
+            else:
+                available_species.append(re.sub("'", '',values[0]))
+    return list(set(available_species))
 
 
 class SearchSens(widgets.VBox):
     ind_title = widgets.HTML(value="""<hr>""")
+    species_title = widgets.HTML(value="""<b>1. Observations</b><br>Species """)
+    locations_title = widgets.HTML(value="""<b>2. Locations</b><br>Choose from the list of locations where observations are available.""")
+    info = widgets.HTML(value='<p style="color:green;">Available: ')
 
     def __init__(self):
 
@@ -34,16 +52,20 @@ class SearchSens(widgets.VBox):
             description="Range_dates",
             style=style,
         )
-        self.location = widgets.SelectMultiple(
-            options=search_locations(),
-            description="Locations",
-            description_tooltip='Multiple values can be selected with "Shift" and/or "ctrl"(or "command")',
-            rows=12,
+        self.location = widgets.TagsInput(
+            allowed_tags=search_locations(['JFJ']),
+            allow_duplicates=False,
             style=style,
         )
         self.domain = widgets.Dropdown(
             options=["EUROPE"],
             description="Domain",
+            ensure_option=True,
+            style=style,
+        )
+        self.time_step = widgets.Dropdown(
+            options=['3hourly','4hourly','6hourly','12hourly','24hourly'],
+            description="Time steps",
             ensure_option=True,
             style=style,
         )
@@ -59,23 +81,36 @@ class SearchSens(widgets.VBox):
             ensure_option=True,
             style=style,
         )
-        self.species = widgets.Dropdown(
-            options=search_species(),
-            description="Species",
-            ensure_option=True,
+        self.species = widgets.TagsInput(
+            value=['inert'], 
+            allowed_tags = search_species(),
+            allow_duplicates=False,
             style=style,
         )
 
-        search_crit = widgets.HBox(
+        self.time_step.observe(self.available_observations,names="value")
+        self.species.observe(self.available_observations,names="value")
+        self.available_obs_list = []
+        
+
+        search_crit = widgets.VBox(
             [
+                widgets.VBox([
+                              self.species_title,
+                              self.species,
+                              self.time_step,
+                              self.domain],
+                              ),
+                self.info,
+                self.ind_title,
+                self.locations_title,
                 self.location,
+                self.ind_title,
                 widgets.GridBox(
                     [
                         self.date_range,
-                        self.domain,
                         self.model,
                         self.model_version,
-                        self.species,
                     ],
                     layout=widgets.Layout(grid_template_columns="repeat(2, 50%)"),
                 ),
@@ -104,6 +139,25 @@ class SearchSens(widgets.VBox):
             ]
         )
 
+    def available_observations(self,change=None):
+        self.info.value = '<p style="color:green;">Available: '
+        available_locations = []
+        qb = QueryBuilder()
+        qb.append(NETCDF, 
+                filters={
+                "attributes.filename": {"ilike": f"%{self.time_step.value}%"},
+                "attributes.global_attributes.species":
+                    {"or": [{"like": f"'%{s}%'"} for s in self.species.value]},
+                },
+                project="attributes.filename")
+        for i in qb.all():
+            s = re.split('_|-',i[0])
+            available_locations.append(s[0])
+        self.info.value += ', '.join(available_locations)
+        self.info.value += '</p>'
+        self.available_obs_list = available_locations
+        self.location.allowed_tags = search_locations(self.available_obs_list)
+
     def accordions(self):
         icons = {True: "  ✅", False: "  ❌"}
         list_locations = {}
@@ -126,6 +180,7 @@ class SearchSens(widgets.VBox):
             i += 1
 
     def search(self, n_location):
+        
         missing_dates = []
         dates_list = utils.simulation_dates_parser([self.date_range.value])
         reformated_dates = list(set([i[:4] + i[5:7] for i in dates_list]))
@@ -141,11 +196,11 @@ class SearchSens(widgets.VBox):
                 "attributes.filename": {
                     "and": [
                         {"or": [{"like": f"{n_location}%"}]},
-                        {"or": [{"like": f"%{l}%"} for l in reformated_dates]},
+                        #{"or": [{"like": f"%{l}%"} for l in reformated_dates]},
                     ]
                 },
                 # "attributes.global_attributes.domain": {"==": f"'{self.domain.value}'"},
-                "attributes.global_attributes.model": {"==": f"'{self.model.value}'"},
+                #"attributes.global_attributes.model": {"==": f"'{self.model.value}'"},
                 # "attributes.global_attributes.model_version": {"ilike": f"'{self.model_version.value}'"},
                 "attributes.global_attributes.species": {
                     "ilike": f"{self.species.value}"
