e52fe22664902a0b453aff450c97a9a1751fda19
LucR31 <94859181+LucR31@users.noreply.github.com>
2024-09-27 08:25:36 +0000

    small changes

--- widgets/code_setup.py
+++ widgets/code_setup.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
 import ipywidgets as widgets
 import aiidalab_widgets_base as awb
+from utils import utils
 
 
 class CodeSetup(widgets.VBox):
@@ -9,13 +11,46 @@ class CodeSetup(widgets.VBox):
                        and or computers.""",
     )
 
-    def __init__(self):
+    def __init__(self, plugin_name: str, new_code=False):
+
         comp_res = awb.ComputationalResourcesWidget()
-        comp_res.code_select_dropdown.layout.visibility = "hidden"
+        self.codes = widgets.Dropdown(
+            description="code",
+            options=utils.codes_list() + ["None"],
+            value=utils.fix_values(plugin_name),
+            style={"description_width": "initial"},
+        )
+
+        self.stash_address = widgets.Text(
+            description="Stash Address",
+            value=utils.fix_values("stash_address"),
+            style={"description_width": "initial"},
+            layout=widgets.Layout(width="40%"),
+        )
+        # self.codes.observe()
+        self.default_button = widgets.Button(
+            description="Save as default", button_style="info"
+        )
+
+        if new_code:
+            comp_res.code_select_dropdown.layout.visibility = "hidden"
+
+        def on_click(b):
+            utils.update_codes(
+                {
+                    plugin_name: self.codes.value,
+                    "stash_address": self.stash_address.value,
+                }
+            )
+
+        self.default_button.on_click(on_click)
 
         self.children = [
             self.new_code_title,
-            comp_res,
+            widgets.HBox(
+                children=[comp_res] * new_code
+                + [self.codes, self.default_button, self.stash_address] * (not new_code)
+            ),
         ]
 
         super().__init__(children=self.children)
